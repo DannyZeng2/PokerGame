d024207ee1f236b6bad8aa195683a317a2a4b2f6
DannyZeng <15625140174@163.com>
2019-08-11 01:15:23 +0800

    [feat] create card entity

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,27 @@
 public class Card {
 
+    private String number;
+    private String suit;
+
+    public Card(String number, String suit) {
+        this.number = number;
+        this.suit = suit;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
 }
