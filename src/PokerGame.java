41a5897438fe0f27dfd8328391e43c6a5a8238ea
DannyZeng <15625140174@163.com>
2019-08-11 01:55:17 +0800

    [test] compare the biggest number ignore suits

--- src/PokerGame.java
+++ src/PokerGame.java
@@ -1,2 +1,21 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PokerGame {
+
+    public static String play(List<Card> cards_1, List<Card> cards_2) {
+
+        List<Card> cards1 = cards_1.stream().sorted(Comparator.comparing(Card::getNumber)).collect(Collectors.toList());
+        List<Card> cards2 = cards_2.stream().sorted(Comparator.comparing(Card::getNumber)).collect(Collectors.toList());
+
+        int bigNum1 = Integer.parseInt(cards1.get(4).getNumber());
+        int bigNum2 = Integer.parseInt(cards2.get(4).getNumber());
+
+        if(bigNum1 == bigNum2) {
+            return "Dogfall";
+        }
+        return bigNum1 > bigNum2 ? "The First Player Win!":"The Second Player Win!";
+
+    }
 }
--- test/PokerGameTest.java
+++ test/PokerGameTest.java
@@ -1,2 +1,28 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class PokerGameTest {
+
+    private static final Card TWO_H = new Card("2", "H");
+    private static final Card THREE_D = new Card("3", "D");
+    private static final Card FOUR_S = new Card("4", "S");
+    private static final Card FIVE_H = new Card("5", "H");
+    private static final Card SIX_C = new Card("6", "C");
+    public static final Card SEVEN_D = new Card("7", "D");
+    private static final Card EIGHT_S = new Card("8", "S");
+    private static final Card NINE_C = new Card("9", "C");
+
+    @Test
+    public void should_return_woinner_when_compare_according_to_big_number() {
+        //Given
+        List<Card> cards_1 = Arrays.asList(SIX_C, TWO_H, THREE_D, FOUR_S, FIVE_H);
+        List<Card> cards_2 = Arrays.asList(THREE_D, NINE_C, FOUR_S, EIGHT_S, TWO_H);
+        //When
+        String result = PokerGame.play(cards_1, cards_2);
+        //Then
+        Assert.assertEquals("The Second Player Win!",result);
+    }
 }
