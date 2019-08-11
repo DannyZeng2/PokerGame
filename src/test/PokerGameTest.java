dc6befc95eeba8f1a8a0d0d9562a379b1cb70a03
DannyZeng <15625140174@163.com>
2019-08-11 13:25:59 +0800

    [test] compare the biggest number include TJQKA ignore suits

--- src/test/PokerGameTest.java
+++ src/test/PokerGameTest.java
@@ -18,8 +18,11 @@ public class PokerGameTest {
     private static final Card SEVEN_D = new Card("7", "D");
     private static final Card EIGHT_S = new Card("8", "S");
     private static final Card NINE_C = new Card("9", "C");
-    private static final Card Jack_H = new Card("J", "K");
+    private static final Card Ten_C = new Card("10", "C");
+    private static final Card Jack_H = new Card("J", "H");
     private static final Card Queen_S = new Card("Q", "S");
+    private static final Card King_D = new Card("K", "D");
+    private static final Card Ace_D = new Card("A", "S");
 
 
     @Test
@@ -27,6 +30,17 @@ public class PokerGameTest {
         //Given
         List<Card> cards_1 = Arrays.asList(SIX_C, TWO_H, THREE_D, FOUR_S, FIVE_H);
         List<Card> cards_2 = Arrays.asList(THREE_D, NINE_C, FOUR_S, EIGHT_S, TWO_H);
+        //When
+        String result = PokerGame.play(cards_1, cards_2);
+        //Then
+        Assert.assertEquals("The Second Player Win!",result);
+    }
+
+    @Test
+    public void should_return_winner_when_compare_big_number_include_TJQKA() {
+        //Given
+        List<Card> cards_1 = Arrays.asList(SIX_C, Jack_H, Ten_C, King_D, FIVE_H);
+        List<Card> cards_2 = Arrays.asList(Queen_S, Ace_D, FOUR_S, EIGHT_S, TWO_H);
         //When
         String result = PokerGame.play(cards_1, cards_2);
         //Then
